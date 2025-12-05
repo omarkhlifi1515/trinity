b2bfe20b954f3d21926f131aab29a0dfdab023ae
omarkhlifi1515 <omarkhlifi1001@gmail.com>
2025-12-05 23:31:37 +0100

    1

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,24 +1,8 @@
 import './globals.css'
-
-export const metadata = {
-  title: 'Trinity - Enterprise Hub',
-  description: 'Internal Enterprise Hub',
-}
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  )
-}
-import './globals.css'
 import React from 'react'
 
 export const metadata = {
-  title: 'Trinity',
+  title: 'Trinity - Enterprise Hub',
   description: 'Trinity Enterprise Hub',
 }
 
